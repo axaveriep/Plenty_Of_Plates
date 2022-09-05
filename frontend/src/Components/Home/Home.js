76fbdbd6149b388e3b1f5b5635579f0985b51c18
Mitchell Grant <mitcho713@gmail.com>
2022-09-05 12:33:08 -0500

    Added Navbar and basic css

--- frontend/src/Components/Home/Home.js
+++ frontend/src/Components/Home/Home.js
@@ -1,18 +1,26 @@
-import {Link} from 'react-router-dom'
+import React from 'react'
 
 export default function Home(props) {
     return(
-        <div>
-            <div className="home--search">
-                {/*restaurant image*/}
-                {/*button or search bar - search for restaurants*/}
-                <input placeholder='Search...'/>
-            </div>
-            <div className="home--create-event">
-                {/*event image*/}
-                {/*button - create event*/}
-                <button>Create Event</button>
-            </div>
-        </div>
+           <div> 
+            <h1>
+            Plenty of Plates 
+            <p>Welcome back User,
+                Just pick a place!
+            </p>
+             </h1>
+            
+             
+             <div className="home--search">
+                 {/*restaurant image*/}
+                 {/*button or search bar - search for restaurants*/}
+                 <input placeholder='Search...'/>
+             </div>
+             <div className="home--create-event">
+                 {/*event image*/}
+                 {/*button - create event*/}
+                 <button>Create Event</button>
+             </div>
+         </div>
     )
-}
+};
---
+++ frontend/src/Components/Navbar/Navbar..js
@@ -0,0 +1,23 @@
+import "./Navbar.css"
+
+
+
+export default function Navbar() {
+    return ( 
+    <nav className="nav">
+        <a href="/" className="site-title">Plenty Of Plates</a>
+        <ul>
+            <li>
+            <a href="home">Home</a>
+            </li>
+            <li>
+            <a href="profile">Profile</a>
+            </li>
+            <li>
+            <a href="logout">Logout</a>
+            </li>
+        </ul>
+    </nav>
+
+    )
+};
