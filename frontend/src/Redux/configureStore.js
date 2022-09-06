d84e7157472780dc2428e29622840e4f8c6899a1
Christian_Boettcher <myarmyemail3547@gmail.com>
2022-09-06 14:18:53 -0400

    added redux-persist

--- frontend/src/Redux/configureStore.js
+++ frontend/src/Redux/configureStore.js
@@ -1,16 +1,46 @@
+// import { createStore, combineReducers, applyMiddleware } from 'redux'
+// import thunk from 'redux-thunk'
+// import {Token} from './token'
+// import {User} from './user'
+
+// export const ConfigureStore = () => {
+//     const store = createStore(
+//         combineReducers({
+//             token: Token,
+//             user: User
+//         }),
+//         applyMiddleware(thunk)
+//     );
+
+//     return store;
+// }
+
+
 import { createStore, combineReducers, applyMiddleware } from 'redux'
 import thunk from 'redux-thunk'
 import {Token} from './token'
 import {User} from './user'
 
-export const ConfigureStore = () => {
-    const store = createStore(
-        combineReducers({
-            token: Token,
-            user: User
-        }),
-        applyMiddleware(thunk)
+import {persistStore, persistReducer} from 'redux-persist'
+import storage from 'redux-persist/lib/storage'
+
+const persistConfig={
+    key:'main-root',
+    storage,
+}
+
+const persistedReducer=persistReducer(persistConfig, 
+    combineReducers({
+        token: Token,
+        user: User
+    })
+);
+
+const store = createStore(
+    persistedReducer,
+    applyMiddleware(thunk)
     );
 
-    return store;
-}
+const Persistor = persistStore(store)
+export {Persistor};
+export default store;
