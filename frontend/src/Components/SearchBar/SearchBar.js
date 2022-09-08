162c10ac5e853957de355a50ec64073a9d958ce2
Ashley <ashley.peebles@gmail.com>
2022-09-08 12:30:25 -0400

    yelp api backend and search bar component

---
+++ frontend/src/Components/SearchBar/SearchBar.js
@@ -0,0 +1,49 @@
+import React, { useState } from 'react'
+import './SearchBar.css'
+import { searchRestaurantsByLocation } from './SearchFunctions'
+
+function SearchBar(props) {
+
+    const [input, setInput] = useState('')
+   // const [searchTerm, setSearchTerm] = useState('')
+
+    const handleChange = (event) => {
+        setInput(event.target.value)
+    }
+
+    const handleKeyDown = (event) => {
+        if(event.key === 'Enter') {
+            
+            handleSearch(event.target.value)
+        }
+    }
+
+    const handleSearch = (searchTerm) => {
+        
+        
+
+        switch(props.searchType) {
+            case 'RESTAURANTS_LOCATION':
+                searchRestaurantsByLocation(searchTerm)
+
+            default: console.log(searchTerm)    
+        }
+    }
+
+
+    return (
+        <div className="search">
+            <div className="search--input">
+                <input type="text" 
+                placeholder="Search..." 
+                value={input}
+                onChange={handleChange}
+                onKeyDown={handleKeyDown}
+                />
+                {/* <div className="search--icon" onClick={handleSearch}>🔍</div> */}
+            </div>
+        </div>
+    )
+}
+
+export default SearchBar
