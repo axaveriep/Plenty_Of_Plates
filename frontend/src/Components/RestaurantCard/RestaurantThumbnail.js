e355b011ed966bb589bd904b3ccb51907d2b6b97
Christian_Boettcher <myarmyemail3547@gmail.com>
2022-09-15 18:07:22 -0400

    refactored and add banner txt to spring

--- frontend/src/Components/RestaurantCard/RestaurantThumbnail.js
+++ frontend/src/Components/RestaurantCard/RestaurantThumbnail.js
@@ -4,30 +4,16 @@ import './RestaurantThumbnail.css'
 export default function RestaurantThumbnail(props) {
 
   return (
-    <div>
-      { props.eventCreated ?
-      <div className="restaurant--thumbnail">
-      <img className="restaurant--thumbnail-image" src={props.restaurant.image_url}/>
-        <span class="text">{props.restaurant.name}</span>
-        <span class="icon">
-        </span>
-      
-    </div>
-
-:
     <div className="restaurant--thumbnail">
       <img className="restaurant--thumbnail-image" src={props.restaurant.image_url}/>
-
-      <button className="restaurant--thumbnail-button" onClick={(event) => props.removeRestaurant(props.restaurant.id)}>
-        <span class="text">{props.restaurant.name}</span>
-        <span class="icon">
+        {props.eventCreated ? <><span class="text">{props.restaurant.name}</span></> : <button className="restaurant--thumbnail-button" onClick={(event) => props.removeRestaurant(props.restaurant.id)}>
+          <span class="text">{props.restaurant.name}</span>
+          <span class="icon">
           <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
             <path d="M24 20.188l-8.315-8.209 8.2-8.282-3.697-3.697-8.212 8.318-8.31-8.203-3.666 3.666 8.321 8.24-8.206 8.313 3.666 3.666 8.237-8.318 8.285 8.203z"></path>
           </svg>
-        </span>
-      </button>
-    </div>
-}
+          </span>
+        </button>}
     </div>
   )
 }
