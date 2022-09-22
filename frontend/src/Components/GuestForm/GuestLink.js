32c1b07beb351e109b463a2e932650c6c3db4a99
Christian_Boettcher <myarmyemail3547@gmail.com>
2022-09-22 19:41:15 -0400

    add copy icon

--- frontend/src/Components/GuestForm/GuestLink.js
+++ frontend/src/Components/GuestForm/GuestLink.js
@@ -11,7 +11,9 @@ export default function GuestLink({guestName, guestId, guestEmail, eventId}) {
     <div className="event--confirmed-guest">
       <label className='guest--name-label'>{guestName}</label>
       <input className="guest--name-input" id="input--eventLink" type='text' readOnly={true} value={`localhost:3000/vote/${eventId}/${guestId}`} />
-      <button id="copyBtn" className='copyBtn' onClick={() => navigator.clipboard.writeText(`localhost:3000/vote/${eventId}/${guestId}`)}>Copy</button>
+      <button id="copyBtn" className='copyBtn' onClick={() => navigator.clipboard.writeText(`localhost:3000/vote/${eventId}/${guestId}`)}>
+        <i className='fa fa-clone'/>
+      </button>
       <UncontrolledTooltip target="copyBtn" trigger="click" autohide={true} placement='top'>Copied!</UncontrolledTooltip>
       {guestEmail === "" ? <></> : guestEmail === undefined ? <></> : 
       <a className="emailLink" href={`mailto:${guestEmail}?&subject=${username} has invited you out!&body=Click this link localhost:3000/vote/${eventId}/${guestId}`} target="_blank" rel="noreferrer">
